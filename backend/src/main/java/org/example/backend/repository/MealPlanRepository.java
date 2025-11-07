aaf8352ebd121f3784c640105f6f0676ec4d198e
ElenaGil <elenagillich@web.de>
2025-11-07 11:32:47 +0100

    remove method

--- backend/src/main/java/org/example/backend/repository/MealPlanRepository.java
+++ backend/src/main/java/org/example/backend/repository/MealPlanRepository.java
@@ -3,8 +3,4 @@ package org.example.backend.repository;
 import org.example.backend.model.MealPlan;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
-public interface MealPlanRepository extends MongoRepository<MealPlan, String> {
-    List<MealPlan> getById(String id);
-}
+public interface MealPlanRepository extends MongoRepository<MealPlan, String> { }
