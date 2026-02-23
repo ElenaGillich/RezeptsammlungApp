6ea0acd8a1009e54d5046e31f45019ebb7380f7b
ElenaGil <elenagillich@web.de>
2026-02-23 17:34:35 +0100

    Update PageTitel. Some styling adjustments.

--- frontend/src/components/pageTitle/PageTitle.tsx
+++ frontend/src/components/pageTitle/PageTitle.tsx
@@ -1,28 +1,52 @@
 import Spinner from "../spinner/Spinner.tsx";
+import SaveButton from "../saveButton/SaveButton.tsx";
 
 type PageTitleProps = {
     hasSpinner?: boolean;
+    hasSaveButton?: boolean;
+    hasMealPlanCreationButton?: boolean;
+    isButtonDisabled?: boolean;
+    hasButtonSpinner?: boolean;
     title: string | undefined;
     hasAdditionalText?: boolean;
     additionalText?: string;
+    onClick?: () => void;
 }
 
 export default function PageTitle(props: Readonly<PageTitleProps>) {
-    const {hasSpinner, title, hasAdditionalText, additionalText} = props
+    const {
+        hasSpinner, hasSaveButton, hasMealPlanCreationButton, title,
+        hasAdditionalText, additionalText, hasButtonSpinner, isButtonDisabled
+    } = props;
 
     return (
-        <div className="page-title-box">
-            <div className="center for-spinner-in-page-title">
-                {hasSpinner && <Spinner/>}
-            </div>
-
-            <div className="section">
-                <div className="page-title">
-                    {title ?? " "}
+        <div className="display-flex items-center under-header">
+            <div className="page-title-box">
+                <div className="center for-spinner-in-page-title">
+                    {hasSpinner && <Spinner/>}
                 </div>
 
-                {(hasAdditionalText && !hasSpinner) && <div className="error">{additionalText}</div>}
+                <div className="section">
+                    <div className="page-title">
+                        {title ?? " "}
+                    </div>
+
+                    {(hasAdditionalText && !hasSpinner) && <div className="error">{additionalText}</div>}
+                </div>
             </div>
+
+            {hasSaveButton &&
+                <SaveButton isDisabled={isButtonDisabled as boolean} hasSpinner={hasButtonSpinner as boolean}/>
+            }
+            {hasMealPlanCreationButton &&
+                <button
+                    className="custom-button"
+                    disabled={isButtonDisabled}
+                    onClick={() => props.onClick?.()}
+                >
+                    {isButtonDisabled ? "Erstellung..." : "Neu erstellen"}
+                </button>
+            }
         </div>
     )
 }
